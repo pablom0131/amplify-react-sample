b56677519c798d167fdbcfeb62c7145a021a9265
Pablo Martinez Castro <pablomc3102@gmail.com>
2024-05-01 18:12:25 -0700

    Removed redundant declarations

--- src/App.js
+++ src/App.js
@@ -3,21 +3,6 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
-import {
-  Button,
-  Flex,
-  Heading,
-  Text,
-  TextField,
-  View,
-  withAuthenticator,
-} from "@aws-amplify/ui-react";
-import { listNotes } from "./graphql/queries";
-import {
-  createNote as createNoteMutation,
-  deleteNote as deleteNoteMutation,
-} from "./graphql/mutations";
-import { generateClient } from 'aws-amplify/api';
 import { API, Storage } from 'aws-amplify';
 import {
   Button,
@@ -29,6 +14,12 @@ import {
   View,
   withAuthenticator,
 } from '@aws-amplify/ui-react';
+import { listNotes } from "./graphql/queries";
+import {
+  createNote as createNoteMutation,
+  deleteNote as deleteNoteMutation,
+} from "./graphql/mutations";
+import { generateClient } from 'aws-amplify/api';
 
 const client = generateClient();
 
